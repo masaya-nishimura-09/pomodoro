ba55ee2c765a68dda96eac739086636769aff992
Masaya <masaya.nishimura0927@gmail.com>
2025-12-10 14:15:55 +0900

    complete

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/masaya-nishimura-09/pomodoro
+
+go 1.22.2
+
+require (
+	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,29 +3,56 @@ package main
 import (
     "fmt"
     "time"
-    "strings"
     "github.com/gosuri/uilive"
+    "strconv"
 )
 
 type Session struct {
+    count int
 }
 
 func main() {
-    time := time.Now()
-    fmt.Println("Current time is:", timeToString(time))
+    session := Session{}
+    for {
+        session.AddCount()
+        switch session.count {
+        case 1, 3, 5, 7:
+            timer(24, "Time to focus")
+        case 2, 4, 6:
+            timer(4, "Short break")
+        case 8:
+            timer(14, "Long break")
+            session.count = 0
+        }
+    }
+}
 
-    for i := 0; i <= 100; i++ {
-        fmt.Fprintf(writer, "Downloading.. (%d/%d) GB\n", i, 100)
-        time.Sleep(time.Millisecond * 5)
+func (s *Session) AddCount() {
+    s.count++
+}
+
+func timer(minutes int, status string) {
+    min := minutes
+    sec := 59
+
+    writer := uilive.New()
+    writer.Start()
+
+    for m := min; m >= 0; m-- {
+        for s := sec; s >= 0; s-- {
+            formattedM := strconv.Itoa(m)
+            formattedS := strconv.Itoa(s)
+            if m < 10 {
+                formattedM = "0" + strconv.Itoa(m)
+            }
+            if s < 10 {
+                formattedS = "0" + strconv.Itoa(s)
+            }
+
+            fmt.Fprintf(writer, "%s %s:%s\n", status, formattedM, formattedS)
+            time.Sleep(time.Second)
+        }
     }
 
-    fmt.Fprintln(writer, "Finished: Downloaded 100GB")
     writer.Stop()
 }
-
-func timeToString(t time.Time) string {
-    formattedTime := t.Format("2006-01-02 15:04:05")
-    currentTimeString := strings.Split(formattedTime, " ")[1]
-    return currentTimeString
-}
-
