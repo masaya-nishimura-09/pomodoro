425a8fbe41729d24e47e8772014a535bc4ad43ea
Masaya <masaya.nishimura0927@gmail.com>
2026-02-14 11:13:54 +0900

    add: table

--- main.c
+++ main.c
@@ -11,16 +11,37 @@
 typedef struct  {
     int progress;
     int min;
-    char title[13];
+    char title[20];
+    char notify_title[13];
     char message[30];
 } Status;
+
+void table(int y1, int x1, int y2, int x2)
+{
+    mvhline(y1, x1, 0, x2-x1);
+    mvhline(y2, x1, 0, x2-x1);
+    mvvline(y1, x1, 0, y2-y1);
+    mvvline(y1, x2, 0, y2-y1);
+    mvhline(2, x1, 0, x2-x1);
+    mvvline(1, 14, 0, 1);
+
+    mvaddch(y1, x1, ACS_ULCORNER);
+    mvaddch(y2, x1, ACS_LLCORNER);
+    mvaddch(y1, x2, ACS_URCORNER);
+    mvaddch(y2, x2, ACS_LRCORNER);
+    mvaddch(2, x1, ACS_LTEE);
+    mvaddch(2, x2, ACS_RTEE);
+}
 
 void timer(Status *status)
 {
     for (int i = status->min; i >= 0 ; i--) {
         for (int j = 59; j >= 0 ; j--) {
-            mvprintw(0, 0, "%s\n", status->title);
-            mvprintw(1, 0, "%02d:%02d\n", i, j);
+            mvprintw(1, 1, "%s\n", status->title);
+            mvprintw(3, 5, "%02d:%02d\n", i, j);
+
+            table(0, 0, 4, 14);
+
             refresh();
             sleep(1);
         }
@@ -29,7 +50,7 @@ void timer(Status *status)
 
 int main(void)
 {
-	initscr();
+    initscr();
     curs_set(0); 
 
     Status status = {0};
@@ -37,17 +58,20 @@ int main(void)
     while (1) {
         switch (status.progress) {
             case 0: case 2: case 4: case 6:
-                strcpy(status.title, "Focus");
+                strcpy(status.title, "    Focus    ");
+                strcpy(status.notify_title, "Focus");
                 strcpy(status.message, "Time to focus!");
                 status.min = 24;
                 break;
             case 1: case 3: case 5:
-                strcpy(status.title, "Short Break!");
+                strcpy(status.title, " Short Break ");
+                strcpy(status.notify_title, "Short Break");
                 strcpy(status.message, "Time for a short break!");
                 status.min = 4;
                 break;
             case 7:
-                strcpy(status.title, "Long Break!");
+                strcpy(status.title, " Long Break  ");
+                strcpy(status.notify_title, "Long Break");
                 strcpy(status.message, "Time for a long break!");
                 status.min = 14;
                 break;
@@ -56,7 +80,7 @@ int main(void)
         /* Show notification */
         NotifyNotification *notif;
         notify_init("Pomodoro");
-        notif = notify_notification_new(status.title, status.message, NULL);
+        notif = notify_notification_new(status.notify_title, status.message, NULL);
         notify_notification_show(notif, NULL);
 
         timer(&status);
